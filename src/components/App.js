0d27f4bae29bb0e9879fdfe0c73d27301ae36c29
Subham Das <104473763+subham8391@users.noreply.github.com>
2023-09-11 09:28:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,33 @@ import '../styles/App.css';
 import InputField from './InputField.js';
 const App = () => {
  
-//code here 
+  const inputRef = useRef(null); 
+  const textareaRef = useRef(null); 
+  const [values, setValues] = useState(''); 
+
+  const settingValue = () => {
+    setValues(inputRef.current.value);
+  };
+
+  const focusInput = () => {
+    inputRef.current.focus();
+  };
+
 
 
  
   return (
     <div>
-    <InputField  ref={newRef}  type="text"/><br/>
-    <button id="settingValueButton" onClick={settingValue}>Set Value</button>
-    <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br/><br/>
-     <textarea id="textarea" value={values}></textarea>
-
+    <InputField ref={inputRef} /> 
+      <br />
+      <button id="settingValueButton" onClick={settingValue}>
+        Set Value
+      </button>
+      <button id="focusInputButton" onClick={focusInput}>
+        Focus the input
+      </button>
+      <br /><br />
+      <textarea id="textarea" value={values} ref={textareaRef}></textarea>
     </div>
   );
 }
